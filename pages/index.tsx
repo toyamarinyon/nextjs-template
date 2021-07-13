478e75c9266c46624c075c2c383b07c200cc33e0
toyamarinyon <toyamarinyon@gmail.com>
2021-07-13 11:46:25 +0900

    add heroicon (#7)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { ThumbUpIcon } from '@heroicons/react/solid'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,7 +14,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to{' '}
+          <a href="https://nextjs.org">
+            Next.js!
+          </a>
+          <ThumbUpIcon className="w-24" />
         </h1>
 
         <p className={styles.description}>
